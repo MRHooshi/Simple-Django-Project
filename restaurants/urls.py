864f73a6ef5277fbe257f07b7cb99f5a3d3352b5
MRH <mr.hooshangi@gmail.com>
2021-05-21 13:55:54 +0430

    restautant urls added.

---
+++ restaurants/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework import routers
+from .views import RestaurantViewSet
+
+router = routers.SimpleRouter()
+router.register('', RestaurantViewSet)
+urlpatterns = [
+    router.urls,
+]
