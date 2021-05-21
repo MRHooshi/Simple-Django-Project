8ea1fbd5aa9bf90df7725506dff0d355ca3d0850
MRH <mr.hooshangi@gmail.com>
2021-05-21 13:50:58 +0430

    migration on restaurant model completed.

--- restaurants/models.py
+++ restaurants/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=20, unique=True)
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
     address = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
